b5ad2858ec96897b0ea204db88f9f0125ec0268b
nahuoo <nahuel.sigismondi@gmail.com>
2020-05-08 23:08:22 -0300

    a

--- src/pages/SeccionFotografias.js
+++ src/pages/SeccionFotografias.js
@@ -15,21 +15,9 @@ const SeccionFotografias = () => {
         })
     })
 
-    let imagenes = React.createRef();
+ 
 
-  React.useEffect(() => {
-    if (window.innerWidth > 991) {
-      const updateScroll = () => {
-        let windowScrollTop = window.pageYOffset / 0.5;
-        imagenes.current.style.transform =
-          "translate3d(0," + windowScrollTop + "px,0)";
-      };
-      window.addEventListener("scroll", updateScroll);
-      return function cleanup() {
-        window.removeEventListener("scroll", updateScroll);
-      };
-    }
-  });
+  
     return (
         <section>
            <Titulo ref={imagenes}>
